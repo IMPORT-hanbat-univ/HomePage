4b1e5a7026129f6c33cba132ad65a629ab794376
devdubu <insomnis2020@naver.com>
2022-05-01 17:26:32 +0900

    ConnectRDS

---
+++ models/data.js
@@ -0,0 +1,27 @@
+module.exports = (sequelize, DataTypes) => {
+	return sequelize.define('data',{
+		title: {
+			type: DataTypes.STRING(20),
+			allowNull: false,
+		},
+		content: {
+			type: DataTypes.TEXT('medium'),
+			allowNull: true,
+		},
+		id_name: {
+			type: DataTypes.STRING(100),
+			allowNull: false,
+		},
+		created_at: {
+			type: DataTypes.DATE,
+			allowNull: false,
+			defaultValue : DataTypes.NOW,
+		},
+        R_degree:{
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        }
+	},{
+		timestamps: false,
+	});
+};
